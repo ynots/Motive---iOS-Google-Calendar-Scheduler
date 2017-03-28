99802572061a84afc3121c6cf6d0d7ad879a0333
Tony Shum <tony.kin.shum@gmail.com>
2017-03-28 16:00:23 -0700

    Initial Commit

---
+++ No Excuses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  No Excuses
+//
+//  Created by Tony Shum on 2017-03-28.
+//  Copyright © 2017 Tony Shum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
